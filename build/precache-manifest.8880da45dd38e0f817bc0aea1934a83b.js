b4f5d7019e87369fa4b5b48882af141a3cf7e07b
Rabie Zaibi <rzaibi@gmail.com>
2020-02-24 03:41:49 -0500

    build ready for production

---
+++ build/precache-manifest.8880da45dd38e0f817bc0aea1934a83b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd6588513ae1c28ec945951df228695e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6abbbdd5ad605f09cb5",
+    "url": "/static/css/main.bfb1d951.chunk.css"
+  },
+  {
+    "revision": "ae3f7160eede41fd94e7",
+    "url": "/static/js/2.57e23102.chunk.js"
+  },
+  {
+    "revision": "4f9cc8a2121e1abaf05ae9a76929ac57",
+    "url": "/static/js/2.57e23102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6abbbdd5ad605f09cb5",
+    "url": "/static/js/main.7b23bfc3.chunk.js"
+  },
+  {
+    "revision": "15533cafc5df199b1ea1",
+    "url": "/static/js/runtime-main.9b066117.js"
+  },
+  {
+    "revision": "fea535a0a5750ae4f523c7baebfb8ca2",
+    "url": "/static/media/login-avatar.fea535a0.png"
+  }
+]);
---
+++ build/precache-manifest.e054d9dbd81577b287085abc793552ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5198171f2497b7a8e6498197df035a7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad32f8442b5efc3ca583",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8da9bfd23eaf3c14ccf2",
+    "url": "/static/css/main.f408a5cd.chunk.css"
+  },
+  {
+    "revision": "ad32f8442b5efc3ca583",
+    "url": "/static/js/2.b4d0e390.chunk.js"
+  },
+  {
+    "revision": "4f9cc8a2121e1abaf05ae9a76929ac57",
+    "url": "/static/js/2.b4d0e390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8da9bfd23eaf3c14ccf2",
+    "url": "/static/js/main.32a5d0a9.chunk.js"
+  },
+  {
+    "revision": "15533cafc5df199b1ea1",
+    "url": "/static/js/runtime-main.9b066117.js"
+  },
+  {
+    "revision": "fea535a0a5750ae4f523c7baebfb8ca2",
+    "url": "/static/media/login-avatar.fea535a0.png"
+  }
+]);
